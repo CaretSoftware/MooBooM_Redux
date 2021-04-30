07651759be2cc34440327c44b7bf238e1a17376d
gretgurt <gretahassler@hotmail.com>
2021-04-30 13:19:44 +0200

    Updated musiccontroller again

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -8,23 +8,31 @@ public class MusicController : MonoBehaviour
 
     [SerializeField] private AudioClip musicClip;
 
-    private bool musicIsPlaying;
+    public static MusicController onlyMusicController;
+
+
 
     private void Awake()
     {
+        if (onlyMusicController == null)
+        {
+            onlyMusicController = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         DontDestroyOnLoad(gameObject);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!musicIsPlaying)
-        {
             audioSource.clip = musicClip;
             audioSource.loop = true;
             audioSource.Play();
-            musicIsPlaying = true;
-        }
     }
 
 
